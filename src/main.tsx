c1db8e571d01bdc9d9945ab140ec6c2211b748de
osmanjosue <osmanjosue@gmail.com>
2024-05-01 15:25:23 -0600

    ReactPro => Seccion 2 => Clase: Primer componente de typeScript

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { Counter } from './bases/Counter'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>React</h1>
-    <hr/>
+    <Counter initialValue={25}/>
   </React.StrictMode>,
 )
